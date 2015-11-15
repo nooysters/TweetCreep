4854c640b4d2b4c807f85eab36f69d1d6681c76c
Jeremy Azzari <jeremy@jeremyazzari.com>
2015-11-15 12:32:32 -0800

    fixed js that parses hashtags

--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -14,14 +14,8 @@ module TwitterClient
   def get_user_tweets(user, num=25)
     Rails.cache.fetch("#{user}/#{num}/tweets", expires_in: 5.minutes) do
       options = {count: num}
-      begin
-        response = twitter_client.user_timeline(user, options)
-      rescue Twitter::Error::NotFound => error
-        return false
-      end
-      response
+      twitter_client.user_timeline(user, options) rescue false
     end
   end
 
-  
 end
